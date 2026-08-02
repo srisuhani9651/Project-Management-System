204c97abfed2332bcdc8ed6ca5ce08c0cf2a5f7a
Suhani Srivastava <srisuhani9651@gmail.com>
2026-08-02 13:46:59 +0530

    create first table user_master to store register and login data

--- Backend/app/models/user_master.py
+++ Backend/app/models/user_master.py
@@ -18,15 +18,10 @@ class UserMaster(Base):
     __table_args__ = {"schema": "auth"}
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-
+    full_name = Column(String(255), nullable=False)
     email = Column(String(255), unique=True, nullable=False)
-    username = Column(String(100), unique=True)
+    username = Column(String(100),nullable=True )
     hashed_password = Column(String(255), nullable=False)
-    full_name = Column(String(255))
-
-    is_active = Column(Boolean, default=True)
-    is_superuser = Column(Boolean, default=False)
-
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(
         DateTime(timezone=True),
